d79ea3886efd7b9a3727a00d244f75305e26e585
Amador Navarro Lucas <macmaker@me.com>
2017-11-10 17:42:38 +0100

    Added project

---
+++ TrackMapper/TrackMapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackMapper
+//
+//  Created by BeRepublic on 10/11/2017.
+//  Copyright © 2017 opentrends. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
